bf2705864a20389005fc5d543573d40f7a27da52
Gergely Laszlo Arpad <gergely.laszlo.a@gmail.com>
2019-07-10 00:55:08 +0300

    Homework 5-Collections_v.1

---
+++ src/PersonFirstNameComparator.java
@@ -0,0 +1,9 @@
+import java.util.Comparator;
+
+public class PersonFirstNameComparator implements Comparator<Person> {
+
+    @Override
+    public int compare(Person o1, Person o2) {
+        return 0;
+    }
+}
